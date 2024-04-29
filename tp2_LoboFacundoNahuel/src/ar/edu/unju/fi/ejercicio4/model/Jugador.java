1160bb7d2edb8604739d722fa0c4ae9d05d5e04b
fffaculobo <facundolobo76@gmail.com>
2024-04-28 23:20:59 -0300

    Subiendo ejercicio 4

---
+++ tp2_LoboFacundoNahuel/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
@@ -0,0 +1,107 @@
+package ar.edu.unju.fi.ejercicio4.model;
+
+import java.time.LocalDate;
+
+import ar.edu.unju.fi.ejercicio4.constantes.Posicion;
+
+public class Jugador {
+	private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    private String nacionalidad;
+    private Integer estatura; // int -> Integer
+    private Integer peso;
+    private Posicion posicion;
+
+    public Jugador(){
+    }
+
+    public Jugador(String nombre, String apellido, LocalDate fechaNacimiento, String nacionalidad, Integer estatura, Integer peso, Posicion posicion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nacionalidad = nacionalidad;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.posicion = posicion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Integer getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(Integer estatura) {
+        this.estatura = estatura;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public int calcularEdad(){
+        LocalDate hoy = LocalDate.now();
+        int edad = hoy.getYear() - fechaNacimiento.getYear();
+        if (hoy.getMonthValue() < fechaNacimiento.getMonthValue() || (hoy.getMonthValue() == fechaNacimiento.getMonthValue() && hoy.getDayOfMonth() < fechaNacimiento.getDayOfMonth())) {
+            edad--;
+        }
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", nacionalidad='" + nacionalidad + '\'' +
+                ", estatura=" + estatura +
+                ", peso=" + peso +
+                ", posicion=" + posicion +
+                ", edad=" + calcularEdad() +
+                '}';
+    }
+}
